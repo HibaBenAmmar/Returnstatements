ee721e40036c1e694179525a7deb930df130f8e5
HibaBenAmmar <86246905+HibaBenAmmar@users.noreply.github.com>
2021-07-08 12:49:34 +0200

    Add files via upload

---
+++ Function return statements/script.js
@@ -0,0 +1,51 @@
+//checking if a number is bigger than 100
+
+const checking = function (number) {
+    const sum = number;
+    if (sum > 100) {
+        return "true";
+               
+    }
+    return "false";
+    
+};
+
+const result = checking (101);
+console.log (result);
+
+//Bouncer at a club...check number of people in club...check age.
+
+
+
+
+//calculating the average
+
+const calculateAverage = function (numbers) {
+const average = numbers.average
+};
+
+const grades = [6,9,5,2,14];
+calculateAverage (grades);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
